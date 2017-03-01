be38e264ec1a8cfe6f032afcca2b128083c7c578
Noel Biscocho <biscochn@uci.edu>
2017-03-01 02:16:05 -0800

    Added simple scene with basic movement for the player and camera.

---
+++ Light-io/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject player;
+
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    private void Update()
+    {
+
+        Vector3 newPosition = player.transform.position; //Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.15f);
+        newPosition.z = -10;
+        transform.position = newPosition;
+    }
+}
---
+++ Light-io/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D m_Rigidbody;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //Move();
+        Move2();
+    }
+
+    public void Move()
+    {
+        Vector3 newPosition = Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.1f);
+        newPosition.z = 0;
+        transform.position = newPosition;
+    }
+    public void Move2()
+    {
+        Vector3 velocity = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        velocity.z = 0;
+        m_Rigidbody.velocity = velocity * 2;
+    }
+
+}
