f4c63f5f212bfc14c936a601cc40da6a938cc45a
Alex Bagg <alexbagg03@gmail.com>
2017-03-02 00:37:47 -0800

    1.) Added a tiled background 2.) Added pickups that randomly spawn across the map when the game starts - each pickup increases the radius of the player's light slightly 3.) Not sure why things aren't being ignored, but maybe after initial pushes/pulls it will work itself out

---
+++ Light-io/Assets/Scripts/Pickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Light>().range += 0.25f;
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Light-io/Assets/Scripts/PickupGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupGenerator : MonoBehaviour {
+
+    public GameObject pickup;
+    public int numberOfPickups = 20;
+    public float mapLength = 100f;
+
+	void Start ()
+    {
+        for (int i = 0; i < numberOfPickups; i++)
+        {
+            Vector2 position = new Vector2(Random.Range(-mapLength, mapLength), Random.Range(-mapLength, mapLength));
+            Instantiate(pickup, position, Quaternion.identity);
+        }
+	}
+
+}
