9404fdf71b074f1c3636328027f413d9752c84b2
Noel Biscocho <biscochn@uci.edu>
2017-03-16 16:34:08 -0700

    Fixed Restarting and Continuing on pressing "A" on controller instead of UI button.

---
+++ Light-io/Assets/Scripts/ContinueScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueScene : MonoBehaviour {
+
+
+    private void Update()
+    {
+        if (enabled && (Input.GetButtonDown("EnterP1") || Input.GetButtonDown("EnterP2")))
+        {
+            GameManager.Instance.LoadNextLevel();
+        }
+    }
+}
