84e71196021ea3ac8f8cd6db596eabfe3f67ca57
Matthew Aguilar <Matthew@dhcp-v054-132.mobile.uci.edu>
2017-03-16 17:28:54 -0700

    Created: black hole generator script to spawn black holes Modified: scene to spawn lots o black holes

--- Light-io/Assets/Scripts/BlackHoleController.cs
+++ Light-io/Assets/Scripts/BlackHoleController.cs
@@ -4,21 +4,8 @@ using UnityEngine;
 
 public class BlackHoleController : MonoBehaviour {
 
-	private float mapSize = 100f; 
 	// Use this for initialization
 	void Start () {
-		randomPosition (); 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	void randomPosition()
-	{
-		Vector2 position = new Vector2 (Random.Range (-mapSize, mapSize), Random.Range (-mapSize, mapSize)); 
-		GameObject.Instantiate (this, position, Quaternion.identity); 
 	}
 
 	void OnTriggerStay2D(Collider2D other)
@@ -26,9 +13,10 @@ public class BlackHoleController : MonoBehaviour {
 		if (other.tag == "Player") {
 			other.GetComponent<PlayerController> ().DecreaseLight (); 
 			other.GetComponent<PlayerController> ().DecreaseLight (); 
+			other.GetComponent<PlayerController> ().DecreaseLight (); 
 		}
 	}
-
+		
 }
 
 
---
+++ Light-io/Assets/Scripts/BlackHoleGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleGenerator : MonoBehaviour {
+
+	public GameObject blackHole;
+	public int blackHolesOnStart = 10;
+	public float mapWidth = 100f;
+
+	void Start ()
+	{
+		for (int i = 0; i < blackHolesOnStart; i++)
+		{
+			spawnBlackHoles();
+		}
+
+	}
+
+	public void spawnBlackHoles()
+	{
+		Vector2 randPosition = new Vector2(Random.Range(-mapWidth, mapWidth), Random.Range(-mapWidth , mapWidth));
+		GameObject.Instantiate(blackHole, randPosition, Quaternion.identity);
+	}
+}
